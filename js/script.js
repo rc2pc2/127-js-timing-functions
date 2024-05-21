4c1f8abf6c18427848ae703921423e07fbe2e6f1
rc2pc2 <petriccarcc@gmail.com>
2024-05-21 11:58:36 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+// console.log('gina va al lago'); // a
+
+// setTimeout( gameOver, 1000, 'timeout!!!'); // ! ASPETTA 3 SECONDI E POI ESEGUI d
+
+// console.log('gino va al mare');// b
+
+// const clock = setInterval( gameOver, 5000, 'interval!!'); // ! OGNI 5 SECONDI ESEGUI e
+
+// console.log('giggina va in montagna'); // c
+
+// function gameOver(points){
+//     console.log(points);
+//     return points;
+// }
