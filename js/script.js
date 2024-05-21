0d5ead4e91abb83412b6f9ca7630f2572759df15
rc2pc2 <petriccarcc@gmail.com>
2024-05-21 12:08:28 +0200

    added new years eve countdown simple example

--- js/script.js
+++ js/script.js
@@ -13,3 +13,22 @@
 //     console.log(points);
 //     return points;
 // }
+// # Chiedo all'utente di inserire un numero, lo converto ad intero
+let userSeconds = Number.parseInt(prompt('Scrivi i secondi che rimangono'), 10);
+
+// # Se l'utente ha richiesto un tempo troppo lungo lo sovrascrivo con 5 secondi
+if (userSeconds > 10 || Number.isNaN(userSeconds)){
+    userSeconds = 5;
+}
+
+// # Inizializzzo una variabile timer alla quale assegno una funzione anonima di callback
+// # da eseguire ogni secondo (1000 millisecondi)
+const timer = setInterval( function(){
+    userSeconds--; // % sottrai un secondo al timer 
+    console.log(userSeconds);
+    if( userSeconds <= 0){ // > se il timer ha raggiunto un numero di secondi uguale o minore a 0
+        clearInterval(timer); // < allora ferma il timer e 
+        alert('BUON ANNO!!!');// > augura buon anno
+    }
+}, 1000);
+
